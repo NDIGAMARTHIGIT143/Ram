d920ada62d2c61964b593310565219b78a87f776
root <root@ip-172-31-26-47.us-east-2.compute.internal>
2020-02-20 17:23:35 +0000

    critical

--- pro.ml
+++ pro.ml
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install apache2 -y
-sudo echo "<h1> Successfully completed Im happy </h1>" > /var/www/html/index.html
+sudo echo "<h1> Successfully completed happy </h1>" > /var/www/html/index.html
 sudo service apache2 restart
