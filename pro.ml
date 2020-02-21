d9fdf2ccff77d20063ca5ea4ebda2e9ae8917a2b
NDIGAMARTHIGIT143 <nbabu045@gmail.com>
2020-02-21 08:13:45 +0530

    tomcat server

--- pro.ml
+++ pro.ml
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install apache2 -y
-sudo echo "<h1> Successfully completed I'm happy </h1>" > /var/www/html/index.html
+sudo echo "<h1> Tomcat deployment </h1>" > /var/www/html/index.html
 sudo service apache2 restart
