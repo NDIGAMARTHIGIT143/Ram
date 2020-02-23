64dc2c77845c0ac672fb9e4c3e5b3a427d3003ec
NDIGAMARTHIGIT143 <nbabu045@gmail.com>
2020-02-23 23:03:07 +0530

    suspence

--- pro.ml
+++ pro.ml
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install apache2 -y 
-sudo echo "<h1>  hello world it was too touf to complete the task <h1>" > /var/www/html/index.html
+sudo apt-get install apache2 -y
+sudo echo "<h1> I think almost done </h1>" > /var/www/html/index.html
 sudo service apache2 restart
-
